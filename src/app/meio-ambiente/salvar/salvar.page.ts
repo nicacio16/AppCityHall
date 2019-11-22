a1a8d75ac8c6d2e36cf1436b9a7750134af916a9
nicacio16 <nicacio16@hotmail.com>
2019-11-21 21:43:13 -0300

    final

--- src/app/meio-ambiente/salvar/salvar.page.ts
+++ src/app/meio-ambiente/salvar/salvar.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MeioAmbiente } from '../meio-ambiente';
 import { AngularFireDatabase } from '@angular/fire/database';
 
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
 @Component({
   selector: 'app-salvar',
   templateUrl: './salvar.page.html',
@@ -11,12 +13,30 @@ export class SalvarPage implements OnInit {
 
   ambiente: MeioAmbiente = new MeioAmbiente();
 
-  constructor(private fire: AngularFireDatabase) { }
+  currentImage: any;
+
+  constructor(private fire: AngularFireDatabase, private camera: Camera) { }
 
   salvar(){
     this.fire.list('ambiente').push(this.ambiente);
     this.ambiente = new MeioAmbiente();
     alert('Solicitação enviada com sucesso!')
+  }
+
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+      console.log("Camera issue:" + err);
+    });
   }
 
   ngOnInit() {
